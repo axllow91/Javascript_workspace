03ee9929d3b96b3c823856dcee9912cf2fd7613c
Dobre Marian <dobremi91@gmail.com>
2018-04-20 21:27:58 +0300

    ES6 EasyHTTP with class implementation

---
+++ CustomEasyHtpp/app.js
@@ -0,0 +1,30 @@
+const http = new EassyHTTP();
+
+// Get Users
+// const users = http.get('http://jsonplaceholder.typicode.com/users')
+//   .then(data => console.log(data))
+//   .catch(error => console.log(error));
+
+// User data
+const data = {
+  name: 'John',
+  userName: 'first_john',
+  email: 'myemail@yadoo.pom'
+};
+
+// Create User
+// const users = http.post('http://jsonplaceholder.typicode.com/users', data)
+//   .then(data => console.log(data))
+//   .catch(error => console.log(error));
+
+// Make an Update (PUT)
+// const userUpdated = 2;
+// const users = http.put(`http://jsonplaceholder.typicode.com/users/${userUpdated}`, data)
+//   .then(data => console.log(data))
+//   .catch(error => console.log(error));
+
+// 
+const users = http.delete('http://jsonplaceholder.typicode.com/users/2')
+  .then(data => console.log(data))
+  .catch(error => console.log(error));
+
---
+++ CustomEasyHtpp/easyhttp2.js
@@ -0,0 +1,86 @@
+/**
+ * EassyHTTP Library
+ * Library for making HTTP Request
+ * 
+ * @version 2.0.0
+ * @author Marian Dobre 
+ * 
+ */
+
+// // Using ES6 classes
+// class EassyHTTP {
+//   // Make an HTTP Get Request
+//   get(url) {
+//     fetch(url)
+//       .then(response => response.json())
+//       .then(data => data)
+//       .catch(error => console.log(error));
+//   }
+// }
+
+// Using Promises
+class EassyHTTP {
+  // Make an HTTP GET Request
+  get(url) {
+    return new Promise((resolve, reject) => {
+      fetch(url)
+        .then(response => response.json())
+        .then(data => resolve(data))
+        .catch(error => reject(error));
+    });
+  }
+
+  // Make an HTTP POST Request
+  post(url, data) {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        // What method we use
+        method: 'POST',
+        headers: {
+          // Content header we want to send
+          'Content-type':'application/json'
+        },
+        // Sending the body converting my javascript value
+        // into a JSON string
+        body: JSON.stringify(data)
+      })
+        .then(response => response.json())
+        .then(data => resolve(data))
+        .catch(error => reject(error))
+    });
+  }
+
+  // Make an HTTP Put Request
+  put(url, data) {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: 'PUT',
+        headers: {
+          'Content-type':'application/json'
+        },
+        // Converting Javascript value into JSON string value
+        body: JSON.stringify(data)
+      })
+        .then(response => response.json())
+        .then(data => resolve(data))
+        .then(error => reject(data))
+    });
+  }
+
+  // Make an HTTP Delete Request
+  delete(url) {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: 'PUT',
+        headers: {
+          'Content-type':'application/json'
+        },
+        // Converting Javascript value into JSON string value
+        body: JSON.stringify(data)
+      })
+        .then(response => response.json())
+        .then(() => resolve('Resource Deleted...'))
+        .then(error => reject(data))
+    });
+  }
+}
